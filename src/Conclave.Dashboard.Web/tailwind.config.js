269c0e14cd3368aed0a336c8b51f0bd3ca8203b3
Alphonzo Escolar <phonz.dev@gmail.com>
2022-11-30 22:01:14 +0800

    Initial version of sidebar and header

--- src/Conclave.Dashboard.Web/tailwind.config.js
+++ src/Conclave.Dashboard.Web/tailwind.config.js
@@ -6,4 +6,12 @@ module.exports = {
         }
     },
     plugins: [],
+    screens: {
+        "xs": "375px",
+        "sm": "414px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1440px",
+        "2xl": "1920px"
+    },
 }
