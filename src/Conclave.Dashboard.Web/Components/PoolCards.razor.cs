0b1d75adb41b778eb7eba48516d5107be0733b47
Pamela Joyce Lacson <lacsonpamelajoyce@gmail.com>
2022-12-01 14:09:08 +0800

    dynamic card designs

--- src/Conclave.Dashboard.Web/Components/PoolCards.razor.cs
+++ src/Conclave.Dashboard.Web/Components/PoolCards.razor.cs
@@ -7,9 +7,27 @@ public partial class PoolCards
 {
   [Parameter]
   public bool IsConclavePool { get; set; }
-  private string CardBorder { get; set; } = "border mud-border-warning";
-  private string CardColor { get; set; } = "mud-theme-warning";
-  private Color ButtonColor { get; set; } = Color.Secondary;
+  private string CardBorder => IsConclavePool switch
+  {
+    true => "border mud-border-warning",
+    false => "border mud-border-primary"
+  };
 
-  
+  private string CardColor => IsConclavePool switch
+  {
+    true => "mud-theme-warning",
+    false => "mud-theme-primary"
+  };
+
+  private Color ButtonColor => IsConclavePool switch
+  {
+    true => Color.Tertiary,
+    false => Color.Secondary
+  };
+
+  private string ButtonBorder => IsConclavePool switch
+  {
+    true => "border-solid border mud-border-tertiary",
+    false => "border-solid border mud-border-primary"
+  };
 }
