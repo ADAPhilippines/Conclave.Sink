592d24946079eedbca31d7705c8f4a178617a72b
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-05 23:26:26 +0800

    change property to init

--- src/Conclave.Sink/Models/Transaction.cs
+++ src/Conclave.Sink/Models/Transaction.cs
@@ -7,5 +7,5 @@ public class Transaction
     public string Hash { get; init; } = string.Empty;
     public ulong Fee { get; init; }
     public IEnumerable<Withdrawal>? Withdrawals { get; init; }
-    public Block? Block { get; init; }
+    public Block Block { get; init; } = new();
 }
