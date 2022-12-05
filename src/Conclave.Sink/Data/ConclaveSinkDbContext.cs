c27699580b76f5483ecb2b230ab8794d80205f45
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-05 22:00:25 +0800

    add transaction entity

--- src/Conclave.Sink/Data/ConclaveSinkDbContext.cs
+++ src/Conclave.Sink/Data/ConclaveSinkDbContext.cs
@@ -15,6 +15,7 @@ public class ConclaveSinkDbContext : DbContext
     public DbSet<PoolDetails> Pools => Set<PoolDetails>();
     public DbSet<WithdrawalByStakeEpoch> WithdrawalByStakeEpoch => Set<WithdrawalByStakeEpoch>();
     public DbSet<StakeByPoolEpoch> StakeByPoolEpoch => Set<StakeByPoolEpoch>();
+    public DbSet<Transaction> Transaction => Set<Transaction>();
 
     public ConclaveSinkDbContext(DbContextOptions<ConclaveSinkDbContext> options) : base(options) { }
 
@@ -30,6 +31,8 @@ public class ConclaveSinkDbContext : DbContext
         modelBuilder.Entity<PoolDetails>().HasKey(s => new { s.Operator, s.TxHash });
         modelBuilder.Entity<WithdrawalByStakeEpoch>().HasKey(wbsea => new { wbsea.StakeAddress, wbsea.Transactionhash });
         modelBuilder.Entity<StakeByPoolEpoch>().HasKey(de => new { de.StakeAddress, de.PoolId, de.TxHash, de.TxIndex });
+        modelBuilder.Entity<Transaction>().HasKey(tx => tx.Hash);
+        modelBuilder.Entity<Transaction>().Property(b => b.Withdrawals).HasColumnType("jsonb");
 
         // Relations
         modelBuilder.Entity<TxInput>()
