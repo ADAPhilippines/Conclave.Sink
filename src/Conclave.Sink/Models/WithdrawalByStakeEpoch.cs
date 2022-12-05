9f05ee3a5b3babffa468c17288d7d6709ccab2c0
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-05 23:26:52 +0800

    change shape of entity to stake address, epoch and amount only

--- src/Conclave.Sink/Models/WithdrawalByStakeEpoch.cs
+++ src/Conclave.Sink/Models/WithdrawalByStakeEpoch.cs
@@ -3,8 +3,7 @@ namespace Conclave.Sink.Models;
 
 public class WithdrawalByStakeEpoch
 {
-    public string StakeAddress { get; set; } = string.Empty;
-    public ulong Amount { get; init; }
-    public string Transactionhash { get; init; } = string.Empty;
-    public Block Block { get; init; } = new();
+    public string? StakeAddress { get; init; }
+    public ulong Epoch { get; init; }
+    public ulong Amount { get; set; }
 }
