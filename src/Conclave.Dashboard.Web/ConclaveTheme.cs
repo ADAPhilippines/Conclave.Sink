1d04288b4a422e0472a2edebca2096582a7a04a3
Pamela Joyce Lacson <lacsonpamelajoyce@gmail.com>
2022-12-06 15:13:29 +0800

    wip button and card colors

--- src/Conclave.Dashboard.Web/ConclaveTheme.cs
+++ src/Conclave.Dashboard.Web/ConclaveTheme.cs
@@ -9,9 +9,9 @@ public class ConclaveTheme : MudTheme
     {
         Palette = new Palette()
         {
-            Primary = Colors.Blue.Accent2, // normal pool card
-            Secondary = Colors.Blue.Accent2, // normal pool button
-            SecondaryDarken ="#2979FF",
+            Primary = Colors.Blue.Accent3, // normal pool card
+            Secondary = Colors.Blue.Accent3, // normal pool button
+            SecondaryDarken ="#2962FF",
             Tertiary = new MudColor("rgba(169, 142, 50, 1)"), // conclave pool button
             TertiaryDarken = "#826d26",
             Warning = new MudColor("rgba(169, 142, 50, 1)"), // conclave pool card
@@ -26,16 +26,16 @@ public class ConclaveTheme : MudTheme
 
         PaletteDark = new PaletteDark()
         {
-            Primary = new MudColor("#259B9B"), // normal pool card
-            Secondary = new MudColor("rgba(65, 251, 251, 0.1)"), // normal pool button
-            SecondaryDarken ="#1b7272",
+            Primary = Colors.Blue.Accent3, // normal pool card
+            Secondary = new MudColor("rgba(41, 121, 255, 0.1)"), // normal pool button
+            SecondaryDarken ="#448AFF",
             Tertiary = new MudColor("rgba(65, 251, 251, 0.1)"), //conclave pool button
             TertiaryDarken = "#1b7272",
             Warning = new MudColor("rgba(169, 142, 50, 1)"), // conclave pool card
             Info = new MudColor("#35B5B5"),
             Success = new MudColor("#079F36"),
             Error = Colors.Red.Darken4,
-            SecondaryContrastText = new MudColor("#41FBFB"),
+            SecondaryContrastText = Colors.Blue.Accent3,
             TertiaryContrastText = new MudColor("#41FBFB"),
             InfoContrastText = new MudColor("#FFFFFF"),
             Surface = new MudColor("rgba(255, 255, 255, 0.5)")
