8f59ca9ad3a7cde89fdbb964aab73e3ce26f22ad
Alphonzo Escolar <phonz.dev@gmail.com>
2022-12-06 10:02:20 +0800

    Add styles on active links (WIP)

--- src/Conclave.Dashboard.Web/Shared/MainLayout.razor.cs
+++ src/Conclave.Dashboard.Web/Shared/MainLayout.razor.cs
@@ -9,4 +9,7 @@ public partial class MainLayout
     private string _walletAddress { get; set; } = "addr_test1qrr86cuspxp7e3cnpcweye...";
 
     private string _walletAddress2 { get; set; } = "addr_test1qr...";
+
+    private string GetActivePageRelativePath() => 
+        NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
 }
