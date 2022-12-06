4686a96f7d92959e7d0aa1e8b0158cbd510ae89a
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-06 22:16:01 +0800

    change property to nullabale

--- src/Conclave.Sink/Models/OuraTransaction.cs
+++ src/Conclave.Sink/Models/OuraTransaction.cs
@@ -4,6 +4,6 @@ namespace Conclave.Sink.Models;
 
 public class OuraTransaction
 {
-    public ulong Fee { get; init; }
-    public IEnumerable<OuraWithdrawal> Withdrawals { get; set; } = new List<OuraWithdrawal>();
+    public ulong? Fee { get; init; }
+    public IEnumerable<OuraWithdrawal>? Withdrawals { get; init; }
 }
