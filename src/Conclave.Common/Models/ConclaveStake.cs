ad554419c499cd87bb8d85f5b7489ff64a696b67
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-09 21:17:52 +0800

    make properties mutable

--- src/Conclave.Common/Models/ConclaveStake.cs
+++ src/Conclave.Common/Models/ConclaveStake.cs
@@ -2,6 +2,6 @@ namespace Conclave.Common.Models;
 
 public record ConclaveStake
 {
-    public ulong Lovelace { get; init; }
-    public ulong Conclave { get; init; }
+    public ulong Lovelace { get; set; }
+    public ulong Conclave { get; set; }
 }
