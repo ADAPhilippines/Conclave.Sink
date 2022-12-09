a7087407f745fb0b99e9af28b786a681260bbfaa
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-09 20:16:06 +0800

    add blockfrost, account service and conclave settings to service dependencies

--- src/Conclave.Sink.Api/Program.cs
+++ src/Conclave.Sink.Api/Program.cs
@@ -1,5 +1,8 @@
+using Blockfrost.Api.Extensions;
+using Conclave.Common.Models;
 using Conclave.Sink.Data;
 using Microsoft.EntityFrameworkCore;
+using ConclaveAccountService = Conclave.Sink.Api.Services.AccountService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,10 @@ builder.Services.AddDbContext<ConclaveSinkDbContext>(options =>
     options.EnableSensitiveDataLogging(true);
     options.UseNpgsql(builder.Configuration.GetConnectionString("ConclaveSink"));
 });
+var blockfrost = builder.Configuration.GetSection("Blockfrost");
+builder.Services.AddBlockfrost(blockfrost.GetValue<string>("Network"), blockfrost.GetValue<string>("ProjectId"));
+builder.Services.AddScoped<ConclaveAccountService>();
+builder.Services.Configure<ConclaveSettings>(builder.Configuration.GetSection("Conclave"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
