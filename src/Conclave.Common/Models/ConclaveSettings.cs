3b0f2f03fb27ea116b01f5f1db5b40ede6bd7e3b
rjlacanlaled <rjlacanlaled@gmail.com>
2022-12-10 17:38:41 +0800

    update settings

--- src/Conclave.Common/Models/ConclaveSettings.cs
+++ src/Conclave.Common/Models/ConclaveSettings.cs
@@ -2,7 +2,7 @@ namespace Conclave.Common.Models;
 
 public record ConclaveSettings
 {
-    public IEnumerable<ConclavePool> Members { get; init; } = new List<ConclavePool>();
+    public IEnumerable<ConclavePool> Members { get; set; } = new List<ConclavePool>();
     public ulong Supply { get; init; }
     public ulong Duration { get; init; }
     public ulong DistributionStart { get; init; }
