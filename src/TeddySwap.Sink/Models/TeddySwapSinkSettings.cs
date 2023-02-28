36e25928688fdf442c4bc538986e9e3b8fae6e92
rjlacanlaled <rjlacanlaled@gmail.com>
2023-02-28 16:48:23 +0800

    add validator addresses to config

--- src/TeddySwap.Sink/Models/TeddySwapSinkSettings.cs
+++ src/TeddySwap.Sink/Models/TeddySwapSinkSettings.cs
@@ -7,4 +7,8 @@ public record TeddySwapSinkSettings
     public ulong EpochLength { get; init; }
     public NetworkType NetworkType { get; init; }
     public IEnumerable<string> Reducers { get; set; } = new List<string>();
+    public string PoolAddress { get; init; }
+    public string DepositAddress { get; init; }
+    public string RedeemAddress { get; init; }
+    public string SwapAddress { get; init; }
 }
