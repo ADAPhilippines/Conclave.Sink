0aed6c61582c0935c494fcb5e50c43a4f9d2217f
Clark Alesna <clark_alesna@hotmail.com>
2023-03-01 12:52:46 +0800

    Responsive refinements

--- src/TeddySwap.UI/Shared/MainLayout.razor.cs
+++ src/TeddySwap.UI/Shared/MainLayout.razor.cs
@@ -5,14 +5,20 @@ namespace TeddySwap.UI.Shared;
 
 public partial class MainLayout
 {
+    protected bool IsDrawerOpen { get; set; } = false;
     protected MudTheme Theme { get; set; } = new()
     {
         PaletteDark = new PaletteDark()
         {
             Primary = new MudColor("rgb(3, 105, 161)"),
             Secondary = new MudColor("rgb(3, 105, 161)"),
+            SecondaryDarken = "rgb(3, 105, 161)",
             Surface = new MudColor("rgb(24, 24, 27)"),
-            Tertiary = new MudColor("rgba(15, 23, 42, 0.5)")
+            Tertiary = new MudColor("rgba(15, 23, 42, 0.5)"),
+            TextPrimary = new MudColor("#FFFFFF"),
+            TextSecondary = new MudColor("#acacac"),
+            DrawerBackground = new MudColor("#1f394d"),
+            DrawerText = new MudColor("#FFFFFF")
         },
         Typography = new Typography()
         {
@@ -22,4 +28,6 @@ public partial class MainLayout
             }
         }
     };
+
+    protected void ToggleDrawer() => IsDrawerOpen = !IsDrawerOpen;
 }
