3481e37a09a20a839da2afe128125e72a1db0db3
rjlacanlaled <rjlacanlaled@gmail.com>
2023-03-08 15:34:26 +0800

    add single address ranking and new migration

---
+++ src/TeddySwap.Sink.Data/Migrations/20230308060336_AddIndex.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeddySwap.Sink.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MainnetAddress",
+                table: "AddressVerifications",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_BatcherAddress",
+                table: "Orders",
+                column: "BatcherAddress");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderType",
+                table: "Orders",
+                column: "OrderType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_Slot",
+                table: "Orders",
+                column: "Slot");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserAddress",
+                table: "Orders",
+                column: "UserAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_BatcherAddress",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderType",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_Slot",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserAddress",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MainnetAddress",
+                table: "AddressVerifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
