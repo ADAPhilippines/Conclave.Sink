5268bb0bc1ea92466266bdbaa81d56eb18485a10
Clark Alesna <clark_alesna@hotmail.com>
2023-04-01 15:48:48 +0800

    added mainnet container settings

--- deployments/docker-entry.sh
+++ deployments/docker-entry.sh
@@ -8,7 +8,7 @@ ASPNETCORE_URLS="http://*:1337" dotnet /bin/TeddySwap.Sink.dll &
 sleep 10
 
 # Start the second process
-/bin/oura daemon --config /config/oura.toml &
+/bin/oura daemon --config /config/oura.$CARDANO_NODE_MAGIC.toml &
   
 # Wait for any process to exit
 wait -n
